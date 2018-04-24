0a4b86bde6f9f80eb077d01809e7c0f82b43e766
KaiZhang <kaizhangbit@gmail.com>
2018-04-23 22:10:27 -0400

    Add file global_mission.yaml and test it.

--- test_a_star_planner.py
+++ test_a_star_planner.py
@@ -1,5 +1,6 @@
 import rospy
 import yaml
+import numpy as np
 from a_star_planner import AStarPlanner
 from robot_model import RobotModel
 import matplotlib.image as mpimg
@@ -10,13 +11,19 @@ rospy.init_node('test_a_star_planner', anonymous=True)
 robot_model = RobotModel(2.0)
 global_planner = AStarPlanner(robot_model)
 
-start = (2, 2)
-goal = (14, 14)
 empty = mpimg.imread('/home/kai/catkin_ws/src/drproj/empty.png')
 global_map_config = yaml.load(open('/home/kai/catkin_ws/src/drproj/empty.yaml'))
 global_map = GridMap(empty, global_map_config['resolution'])
 global_map.show('rviz_global_grid_map')
 global_map.print()
+
+global_mission = yaml.load(open('/home/kai/catkin_ws/src/drproj/global_mission.yaml'))
+start_grid_x = int(global_mission['start'][0] / global_map.resolution + 0.5)
+start_grid_y = int(global_mission['start'][1] / global_map.resolution + 0.5)
+goal_grid_x = int(global_mission['goal'][0] / global_map.resolution + 0.5)
+goal_grid_y = int(global_mission['goal'][1] / global_map.resolution + 0.5)
+start = (start_grid_x, start_grid_y)
+goal = (goal_grid_x, goal_grid_y)
 
 path = global_planner.get_path(start, goal, global_map)
 global_planner.show_path('rviz_global_path', global_map)
--- test_virtual_vehicle.py
+++ test_virtual_vehicle.py
@@ -9,7 +9,9 @@ import matplotlib.image as mpimg
 
 rospy.init_node('test_virtual_vehicle', anonymous=True)
 
-vehicle_state = VehicleState(7.0, 6.5, 0.0)
+global_mission = yaml.load(open('/home/kai/catkin_ws/src/drproj/global_mission.yaml'))
+vehicle_state = VehicleState(global_mission['start'][0], global_mission['start'][1], global_mission['start'][2])
+
 vehicle_model = VehicleModel('/home/kai/catkin_ws/src/drproj/vehicle_config.yaml')
 virtual_vehicle = VirtualVehicle(vehicle_state, vehicle_model, 'vehicle_cmd')
 
